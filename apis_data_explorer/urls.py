658831a98af08412580d6645e0f32eec116ffe5e
Gytha Ogg <saranya.balasubramanian@oeaw.ac.at>
2026-06-23 09:16:14 +0200

    refactor: simplify URL structure

--- apis_data_explorer/urls.py
+++ apis_data_explorer/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from  apis_data_explorer.views import HomeView,EntitySearchView
 
 urlpatterns = [
-    path("", HomeView.as_view(), name="home"),
-    path("search/", EntitySearchView.as_view(), name="search"),
-
+    path("", EntitySearchView.as_view(), name="explore"),
 ]
